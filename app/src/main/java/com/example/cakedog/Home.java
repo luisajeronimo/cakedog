f5abef9967240dea14ca56e5317a7a7f85cca68c
luisajeronimo <luisalima135@gmail.com>
2023-09-15 22:25:36 -0300

    primeira entrega

---
+++ app/src/main/java/com/example/cakedog/Home.java
@@ -0,0 +1,18 @@
+package com.example.cakedog;
+
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class Home extends AppCompatActivity {
+    ImageView imgCarroussel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+}
