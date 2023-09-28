178f0f0ebc7ba561141a29f630d3cd3e93a7a35e
Luisa Lima <luisalima135@gmail.com>
2023-09-27 22:24:17 -0300

    Atualizacao da Home com produtos

--- app/src/main/java/com/example/cakedog/MainActivity.java
+++ app/src/main/java/com/example/cakedog/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-    ImageView imgCarroussel;
+    ImageView imgCarroussel, imgCarroussel2, imgCarroussel3, imgCarroussel4, imgCarroussel5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
 
         imgCarroussel = findViewById(R.id.imgCarroussel);
         imgCarroussel.setImageResource(R.drawable.foto);
+
+        imgCarroussel2 = findViewById(R.id.imgCarroussel2);
+        imgCarroussel2.setImageResource(R.drawable.foto);
+
+        imgCarroussel3 = findViewById(R.id.imgCarroussel3);
+        imgCarroussel3.setImageResource(R.drawable.foto);
+
+        imgCarroussel4 = findViewById(R.id.imgCarroussel4);
+        imgCarroussel4.setImageResource(R.drawable.foto);
+
+        imgCarroussel5 = findViewById(R.id.imgCarroussel5);
+        imgCarroussel5.setImageResource(R.drawable.foto);
     }
 }
